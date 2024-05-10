2937428029f1d931934d0f908cef67cf84b1d311
missyolin eunike <101983657+Missyolin@users.noreply.github.com>
2024-05-10 21:27:52 +0700

    postbukuinternasional

--- app/Http/Controllers/PenelitianController.php
+++ app/Http/Controllers/PenelitianController.php
@@ -131,50 +131,50 @@ class PenelitianController extends Controller
     }
 
     public function postBukuInternasional(Request $request)
-{
-    // Retrieve the id_rencana from the request payload
-    $id_rencana = $request->input('id_rencana');
+    {
+        // Retrieve the id_rencana from the request payload
+        $id_rencana = $request->input('id_rencana');
 
-    // Check if Rencana exists with the provided id_rencana
-    $rencana = Rencana::where('id_rencana', $id_rencana)->first();
+        // Check if Rencana exists with the provided id_rencana
+        $rencana = Rencana::where('id_rencana', $id_rencana)->first();
 
-    if (!$rencana) {
-        return response()->json(['error' => 'Rencana not found'], 404);
-    }
-
-    $id_dosen = $rencana->id_dosen;
-
-    $filenames = [];
-
-    if ($request->hasFile('fileInput')) {
-        $files = $request->file('fileInput');
-        foreach ($files as $file) {
-            if ($file->isValid()) {
-                $extension = $file->getClientOriginalExtension();
-                $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME) . '_' . $id_dosen . '_Buku_Internasional_' . time() . '.' . $extension;
-                $file->move(storage_path('app/documents/penelitian'), $filename);
-                $filenames[] = $filename;
-            } else {
-                continue;
-            }
+        if (!$rencana) {
+            return response()->json(['error' => 'Rencana not found'], 404);
         }
-    } else {
-        return response()->json(['error' => 'No files selected'], 400);
+
+        $id_dosen = $rencana->id_dosen;
+
+        $filenames = [];
+
+        if ($request->hasFile('fileInputD')) {
+            $files = $request->file('fileInputD');
+            foreach ($files as $file) {
+                if ($file->isValid()) {
+                    $extension = $file->getClientOriginalExtension();
+                    $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME) . '' . $id_dosen . '_Buku_Internasional' . time() . '.' . $extension;
+                    $file->move(app()->basePath('storage/documents/penelitian'), $filename);
+                    $filenames[] = $filename;
+                } else {
+                    return response()->json(['error' => 'Something went wrong'], 401);
+                }
+            }
+        } else {
+            return response()->json(['error' => 'No files selected'], 400);
+        }
+
+        // Update $rencana->lampiran with new filenames
+        $rencana->lampiran = is_array($rencana->lampiran) ? $rencana->lampiran : [];
+        $rencana->lampiran = array_merge($rencana->lampiran, $filenames);
+
+        $rencana->save();
+
+        $res = [
+            "rencana" => $rencana,
+            "message" => "Lampiran added successfully"
+        ];
+
+        return response()->json($res, 202);
     }
-
-    // Update $rencana->lampiran with new filenames
-    $rencana->lampiran = is_array($rencana->lampiran) ? $rencana->lampiran : [];
-    $rencana->lampiran = array_merge($rencana->lampiran, $filenames);
-
-    $rencana->save();
-
-    $res = [
-        "rencana" => $rencana,
-        "message" => "Lampiran added successfully"
-    ];
-
-    return response()->json($res, 200);
-}
 
 //method M
 public function getPembicaraSeminar($id)
