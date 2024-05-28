ec0a0f157ddb6864551aa5375e7c670937419ada
boyysitorus <sitorusboy0123@gmail.com>
2024-05-28 17:00:04 +0700

    riwayat controller

---
+++ app/Http/Controllers/RiwayatController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rencana;
+use Illuminate\Http\Request;
+
+class RiwayatController extends Controller
+{
+    public function getPendidikan($idTanggal, $idDosen)
+    {
+        // BAGIAN A
+        $teori = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_kelas', 'detail_pendidikan.jumlah_evaluasi', 'detail_pendidikan.sks_matakuliah', 'rencana.sks_terhitung', 'rencana.lampiran')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'teori')
+            ->get();
+
+        // BAGIAN B
+        $praktikum = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_kelas', 'detail_pendidikan.sks_matakuliah', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'praktikum')
+            ->get();
+
+        // BAGIAN C
+        $bimbingan = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_mahasiswa', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'bimbingan')
+            ->get();
+
+        // BAGIAN D
+        $seminar = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_dosen', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'seminar')
+            ->get();
+
+        // BAGIAN E
+        $tugasAkhir = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_kelompok', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'tugasAkhir')
+            ->get();
+
+        // BAGIAN F
+        $proposal = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_mahasiswa', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'proposal')
+            ->get();
+
+        // BAGIAN G
+        $rendah = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_sap', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'rendah')
+            ->get();
+
+        // BAGIAN H
+        $kembang = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_sap', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'pengembangan')
+            ->get();
+
+        // BAGIAN I
+        $cangkok = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'detail_pendidikan.jumlah_dosen', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'cangkok')
+            ->get();
+
+        // BAGIAN J
+        $koordinator = Rencana::join('detail_pendidikan', 'rencana.id_rencana', '=', 'detail_pendidikan.id_rencana')
+            ->select('rencana.id_rencana', 'rencana.nama_kegiatan', 'rencana.sks_terhitung')
+            ->where("id_tanggal", $idTanggal)
+            ->where("id_dosen", $idDosen)
+            ->where('rencana.sub_rencana', 'koordinator')
+            ->get();
+
+        // Kembalikan data dalam bentuk yang sesuai untuk ditampilkan di halaman
+        return response()->json([
+            'teori' => $teori,
+            'praktikum' => $praktikum,
+            'bimbingan' => $bimbingan,
+            'rendah' => $rendah,
+            'kembang' => $kembang,
+            'cangkok' => $cangkok,
+            'seminar' => $seminar,
+            'koordinator' => $koordinator,
+            'tugasAkhir' => $tugasAkhir,
+            'proposal' => $proposal
+        ], 200);
+    }
+}
